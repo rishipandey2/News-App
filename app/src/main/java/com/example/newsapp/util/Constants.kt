f41b95b35d799dd7f7dbd9a874ae375b9e78456d
rishipandey2 <rishipandeyinfo@gmail.com>
2025-02-01 21:25:28 +0530

    New Project Added

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "802db913576241bfa5caedec60b587a7"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "300L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
